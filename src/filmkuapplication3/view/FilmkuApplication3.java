7d172763587d80da4816e1e8da0236b689496e6f
BennoAlif <bennoalif41@gmail.com>
2019-12-19 12:20:25 +0700

    First init

---
+++ src/filmkuapplication3/view/FilmkuApplication3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filmkuapplication3.view;
+
+/**
+ *
+ * @author Kyoto
+ */
+public class FilmkuApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginFrame login = new LoginFrame();
+        login.setVisible(true);
+    }
+    
+}
